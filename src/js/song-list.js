60abc3f36817967cd50ce29614af304e389a0220
fatfanfan <fatfan1020@gmail.com>
2018-09-10 21:57:21 +0800

    新建时清空form数据

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -24,7 +24,6 @@
     },
     clearActive(){
       $(this.el).find('.active').removeClass('active')
-
     }
   }
   let model = {
@@ -80,6 +79,10 @@
         this.model.data.songs.push(songData)
         this.view.render(this.model.data)
       })
+      window.eventHub.on('new',()=>{
+        this.view.clearActive()
+      })
+
     }
   }
 
