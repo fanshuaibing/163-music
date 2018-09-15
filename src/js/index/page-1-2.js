857fbdb984a07acd8587eccaabe67c76c719b092
fatfanfan <fatfan1020@gmail.com>
2018-09-15 14:59:57 +0800

    添加播放暂停功能

--- src/js/index/page-1-2.js
+++ src/js/index/page-1-2.js
@@ -10,7 +10,7 @@
             </svg>
             {{song.singer}}
           </p>
-          <a class="playButton" href="#">
+          <a class="playButton" href="./song.html?id={{song.id}}">
             <svg class="icon icon-play">
               <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-play"></use>
             </svg>
@@ -26,9 +26,10 @@
         let $li = $(this.template
             .replace('{{song.name}}', song.name)
             .replace('{{song.singer}}', song.singer)
+            .replace('{{song.id}}', song.id)
         )
         this.$el.find('ol.list').append($li)
-        
+
       })
     }
   }
@@ -54,7 +55,6 @@
       this.model.find().then(()=>{
         this.view.render(this.model.data)
       })
-
     }
   }
   controller.init(view, model)
